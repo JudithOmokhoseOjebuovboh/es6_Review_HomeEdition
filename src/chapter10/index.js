c2090c185ff3e5ea268b2b8d8de7839617efe450
JudithOmokhoseOjebuovboh <omoid25@gmail.com>
2019-12-10 10:18:07 +0100

    Committing old format JS(Pre_ES6)

---
+++ src/chapter10/index.js
@@ -0,0 +1,10 @@
+/*(() => {
+    document.getElementById("output").innerHTML = "Hello Chapter 10"
+   })(); index-previous.js*/
+
+var log = function(output){
+    document.getElementById("output").innerHTML = output;
+   };
+   //For illustration, call the function above to log an output
+   log("Welcome to Chapter 10!"); 
+

663d348843beed7d1d180c9b1c3d368d844300cf
Omokhose Judith Ojebuovboh <omoid25@gmail.com>
2020-01-10 02:32:40 +0100

    testss

--- src/chapter10/index.js
+++ src/chapter10/index.js
@@ -1,10 +1,21 @@
 /*(() => {
     document.getElementById("output").innerHTML = "Hello Chapter 10"
-   })(); index-previous.js*/
+   })();
 
 var log = function(output){
     document.getElementById("output").innerHTML = output;
-   };
-   //For illustration, call the function above to log an output
-   log("Welcome to Chapter 10!"); 
+};
+*/
+//For illustration, call the function above to log an output
+/*log("Welcome to Chapter 10!"); 
 
+import log from './tools'
+//For illustration, call the imported log function
+log("Welcome to Chapter 10! We are now having fun with modularity"); 
+*/
+
+/*import log, {appName, dummyFunction} from './tools';
+log("Welcome to Chapter 10! The application name is " + appName + ". There is a function that returns " + dummyFunction());
+
+import logger, {appName, dummyFunction} from './tools';
+logger("Welcome to Chapter 10! The application name is \'" + appName + "\'. There is a function that returns \'" + dummyFunction() + "\'."); */
---
+++ src/chapter10/tools.js
@@ -0,0 +1,9 @@
+var log = function(output){
+    document.getElementById("output").innerHTML = output;
+};
+
+export var appName = "Chapter 10 Code Illustrations";
+export var dummyFunction = function(){
+    return "I am a dummy function";
+}
+export default log; 
